60164aed043bb2e4f732d05a07ad2bca12d80ade
eeeebta <eeeebta@gmail.com>
2020-10-09 21:52:20 -0400

    Finished Alpha v0.0.1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import Flask, render_template, request, Response, redirect
+from werkzeug.exceptions import HTTPException, default_exceptions, InternalServerError
 
 app = Flask(__name__)
 
@@ -9,9 +10,10 @@ app = Flask(__name__)
 def main():
     if request.method == "POST":
         roster = []
-        # TODO make this look nicer
+        # TODO make this look nicer -- done
+        # TODO now add JavaScript to prompt before submission (!!)
         if not request.files.get("roster"):
-            return "roster file not uploaded"
+            return throw_web_error("Roster file not uploaded", 404)
         else:
             roster = [line.decode() for line in request.files.get("roster").readlines()]
             print(roster)
@@ -42,15 +44,15 @@ def main():
 
             # TODO fix error message
             if len(files) < 1:
-                return "Files were not attached"
+                return throw_web_error("Files not attached", 404)
 
             # Otherwise continue on looking at the files and then decode the contents of those files
             merged_files_arr = [contents.decode().lower() for file in files for contents in file.readlines()]
 
-        # Generate function used to return the file, but won't be used here
-        # def generate():
-        #     for file_content in merged_files_arr:
-        #         yield "".join(file_content)
+            # Generate function used to return the file, but won't be used here
+            # def generate():
+            #     for file_content in merged_files_arr:
+            #         yield "".join(file_content)
 
             print("off")
 
@@ -62,9 +64,12 @@ def main():
 
         print(student_dict)
 
+        found_id = False
+
         # TODO might do a recode here cause this is sort of messy
         # Check if the roster file is valid
         processed_file_arr = []
+        split_with_code = ""
         if len(roster) < 1 or roster[0] != "--------ROSTER_HEAD--------\n":
             return "roster is not valid"
         try:
@@ -78,33 +83,52 @@ def main():
             # Process without IDs cause it's not a json/python dict
         except KeyError:
             try:
+
                 print(f"MERGED_ARR: {merged_files_arr}")
                 processed_file_arr = []
                 class_codes = []
                 for key, val in student_dict.items():
                     if key != "NO_ID":
                         class_codes.append(key)
-                        print(val)
+                        student_list = val
 
                 for key in class_codes:
                     for message in merged_files_arr:
                         if key in message:
-                            processed_file_arr.append(message)
+                            # Strip both characters because then this way it works for both windows and mac
+                            processed_file_arr.append(message.split(" : ")[1].strip("\n").strip("\r"))
 
-                print(merged_files_arr)
+                found_id = True
+                split_with_code = {}
+                for code in class_codes:
+                    split_with_code[code] = []
+
+                for code in class_codes:
+                    for student in processed_file_arr:
+                        if code in student:
+                            split_with_code[code].append(student.split(f" {code}")[0])
+
+                print(f"SPLIT_: {split_with_code}")
+                print(f"MERGED_1: {merged_files_arr}")
                 print(f"CCODES: {class_codes}")
-                print(processed_file_arr)
+                print(f"PROCESSED: {processed_file_arr}")
+                print(f"STUDENT_DICT: {student_dict}")
             except KeyError:
                 return "Invalid structure of roster"
+
+        if found_id:
+            print("do the other stuff")
+            absent = get_absent_with_code(student_dict, split_with_code)
+            print(f"ABSENT1: {absent}")
+            print(f"ABSENT1: {absent}")
+            print(f"ABSENT1: {absent}")
+            return render_template("index.html", render_absent_skeleton=True, students=student_list, absent=absent,
+                                   using_dict=found_id)
 
         students = set(processed_file_arr)
         print(f"PROCESSED_SET: {set(processed_file_arr)}")
 
         absent = get_absent(student_list, students)
-        print(f"ABSENT: {absent}")
-        print(f"ABSENT: {absent}")
-        print(f"ABSENT: {absent}")
-        print(f"ABSENT: {absent}")
         print(f"ABSENT: {absent}")
         print(f"ABSENT: {absent}")
 
@@ -113,7 +137,7 @@ def main():
         #                headers={"Content-Disposition": "attachment;filename=test.txt"})
 
         # TODO redirect somewhere else
-        return render_template("index.html", message="E", students=students, absent=absent, using_dict=False)
+        return render_template("index.html", message="E", students=students, absent=absent, using_dict=found_id)
     else:
         return render_template("index.html")
 
@@ -206,7 +230,31 @@ def get_absent(roster, students):
     return absent_students
 
 
-def process_files(roster, attendance_file):
-    # Structure should follow: {"Class": ["Students", "for", "class", "go" "here"], "Class2": []}
+def get_absent_with_code(roster, students):
     absent_students = {}
+    for class_code, student_list in roster.items():
+        absent_students[class_code] = []
+    # TODO BUGFIX: One potential bug is that if a code in the roster is there, but then the code isn't in the student's
+    #  list then that will break this program
+    for class_code, student_list in roster.items():
+        for roster_student in student_list:
+            if roster_student not in students[class_code]:
+                absent_students[class_code].append(roster_student.title())
     return absent_students
+
+
+def throw_web_error(message, e_code=400):
+    """Render message as an apology to user."""
+    return render_template("error.html", top=e_code, bottom=message), e_code
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return throw_web_error(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
